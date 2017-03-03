df7e5ef4933224cacb2a97738eb609b0d2341708
knva <txt_ch@163.com>
2017-03-03 15:04:47 +0800

    #1 connect to asf server simple

---
+++ WCFClient/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WCFClient
+{
+   
+    public partial class Form1 : Form
+    {
+        private static Mwcf Wcf = new Mwcf();
+        public Form1(string a )
+        {
+            InitializeComponent();
+            label1.Text = a;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            string a = Wcf.SendCommand(x);
+            label1.Text = a;
+        }
+    }
+}
---
+++ WCFClient/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+namespace WCFClient
+{
+    [ServiceContract]
+    internal interface IWCF
+    {
+        [OperationContract]
+        string GetStatus();
+
+        [OperationContract]
+        string HandleCommand(string input);
+    }
+    static class Program
+    {
+        private static Mwcf Wcf = new Mwcf();
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Mwcf.Init();
+            string a = Wcf.SendCommand("version");
+            Application.Run(new Form1(a));
+        }
+    }
+
+    internal sealed class Mwcf : IWCF, IDisposable
+    {
+        private Client Client;
+        private static string Host;
+        private static string HostPort;
+        private ServiceHost ServiceHost;
+        private static System.ServiceModel.Channels.Binding GetTargetBinding()
+        {
+            System.ServiceModel.Channels.Binding result;
+
+            result = new WSHttpBinding
+            {
+                // This is a balance between default of 8192 which is not enough, and int.MaxValue which is prone to DoS attacks
+                // We assume maximum of 255 bots and maximum of 1024 characters per each bot included in the response
+                ReaderQuotas = { MaxStringContentLength = byte.MaxValue * 1024 },
+
+                // We use SecurityMode.None for Mono compatibility
+                // Yes, also on Windows, for Mono<->Windows communication
+                Security = { Mode = SecurityMode.None }
+            };
+            
+
+            result.SendTimeout = new TimeSpan(0, 0, 60);
+            return result;
+        }
+        //初始化
+        internal static void Init()
+        {
+
+            Host = "106.184.7.15";
+            HostPort = "1242";
+
+        }
+
+        internal string SendCommand(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                //   ASF.ArchiLogger.LogNullError(nameof(input));
+                return null;
+            }
+
+            System.ServiceModel.Channels.Binding binding = GetTargetBinding();
+            if (binding == null)
+            {
+                //ASF.ArchiLogger.LogNullError(nameof(binding));
+                return null;
+            }
+
+            string url = GetUrlFromBinding(binding);
+            if (string.IsNullOrEmpty(url))
+            {
+                //  ASF.ArchiLogger.LogNullError(nameof(url));
+                return null;
+            }
+
+            //   ASF.ArchiLogger.LogGenericInfo(string.Format(Strings.WCFSendingCommand, input, url));
+
+            if (Client == null)
+            {
+                Client = new Client(
+                    binding,
+                    new EndpointAddress(url)
+                );
+            }
+
+            return Client.HandleCommand(input);
+        }
+        private static string GetUrlFromBinding(System.ServiceModel.Channels.Binding binding)
+        {
+            if (binding != null)
+            {
+                return binding.Scheme + "://" + Mwcf.Host + ":" + Mwcf.HostPort + "/ASF";
+            }
+
+           // ASF.ArchiLogger.LogNullError(nameof(binding));
+            return null;
+        }
+
+        public string GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleCommand(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+    }
+
+
+
+    internal sealed class Client : ClientBase<IWCF>
+    {
+
+        internal Client(System.ServiceModel.Channels.Binding binding, EndpointAddress address) : base(binding, address) { }
+
+        internal string HandleCommand(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+
+                return null;
+            }
+
+            try
+            {
+                return Channel.HandleCommand(input);
+            }
+            catch (Exception e)
+            {
+                //ASF.ArchiLogger.LogGenericException(e);
+                return null;
+            }
+        }
+    }
+}
